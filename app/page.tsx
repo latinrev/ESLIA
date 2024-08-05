5187cac82d2ea5e9a17d54e6b7885e05fc282fc0
latinrev <latinreve@gmail.com>
2024-08-05 17:33:52 -0500

    fix(typos): front-page typos add redirect to production

--- app/page.tsx
+++ app/page.tsx
@@ -6,47 +6,70 @@ import Image from "next/image";
 import Link from "next/link";
 import { Circles } from "react-loader-spinner";
 import { motion, AnimatePresence } from "framer-motion";
-import GoogleIcon from '@/components/GoogleIcon'
+import GoogleIcon from "@/components/GoogleIcon";
 export default function Home() {
-  const supabase = createClient()
-  return <AnimatePresence mode="wait" initial={true}>
-    <div className="flex flex-col gap-8">
-      <motion.div initial={{ y: '-5%', opacity:0 }} animate={{y:0,opacity:1}} className="flex-col flex items-center justify-self-start gap-4 text-center overflow-hidden h-full pb-12 relative md:p-12 ">
-        <h1 className="text-5xl md:text-5xl font-bold md:px-10">Aprende <span className="text-textContrast font-black">Ingles facil</span> con la <span className="text-textContrast font-black">ayuda</span><br /> de la  <span className="text-textContrast font-black">inteligencia artificial</span> 🎉</h1>
-        <h4 className="text-lg md:text-2xl font-medium md:px-10">Con <span className="text-textContrast font-bold"> ELSIA </span>genera hojas de <span className="text-textContrast font-bold">vocabulario</span> y <span className="text-textContrast font-bold">ejercicios</span><br /> personalizadas a tu nivel de dificultad! 🤓</h4>
-        <Button onClick={() => {
-          supabase.auth.signInWithOAuth({
-            provider: "google",
-            options: {
-              redirectTo: 'https://glorious-zebra-qp4655767xjhx5vv-3000.app.github.dev/worksheets',
-            }
-          })
-        }}><span className="flex center gap-2"><GoogleIcon />Iniciar sesion con Google </span></Button>
-      </motion.div>
-      <motion.div  initial={{ y: '10%', opacity:0 }} animate={{y:0,opacity:1}}  className=" grid grid-cols-1 lg:grid-cols-3 xl:px-36 gap-8 ">
-        <Card
-          item={{
-            emoji: '🏄🏼‍♂️',
-            title: "Es facil!",
-            description: "Ya que todo es personalizado puedes aprender los temas que quieras cuando quieras",
-          }} />
-        <Card
-
-          item={{
-            emoji: '🚀',
-            title: "Aprendiendo ingles con ESLIA",
-            description: "Aca podras generar hojas de vocabulario y ejercicios que que te resultara increible para aprender ingles",
-          }} />
-        <Card
-          item={{
-            emoji: '💸',
-            title: "Gratis! ",
-            description: "Puedes generar tantas hojas de ejercicios como quieras",
-          }}></Card>
-      </motion.div>
-    </div >
-  </AnimatePresence>
-
+  const supabase = createClient();
+  return (
+    <AnimatePresence mode="wait" initial={true}>
+      <div className="flex flex-col gap-8">
+        <motion.div
+          initial={{ y: "-5%", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          className="relative flex h-full flex-col items-center gap-4 justify-self-start overflow-hidden pb-12 text-center md:p-12">
+          <h1 className="text-5xl font-bold md:px-10 md:text-6xl">
+            Aprende <span className="font-black text-textContrast">Ingles facil</span> con la{" "}
+            <span className="font-black text-textContrast">ayuda</span> <br /> de la{" "}
+            <span className="font-black text-textContrast">inteligencia artificial</span> 🎉
+          </h1>
+          <h4 className="text-lg font-medium md:px-10 md:text-2xl">
+            Con <span className="font-bold text-textContrast"> ESLIA </span>genera hojas de{" "}
+            <span className="font-bold text-textContrast">vocabulario</span> y{" "}
+            <span className="font-bold text-textContrast">ejercicios</span>
+            <br /> personalizadas a tu nivel de dificultad! 🤓
+          </h4>
+          <Button
+            onClick={() => {
+              supabase.auth.signInWithOAuth({
+                provider: "google",
+                options: {
+                  redirectTo: "https://www.esliafacil.com/worksheets",
+                },
+              });
+            }}>
+            <span className="center flex gap-2">
+              <GoogleIcon />
+              Iniciar sesion con Google{" "}
+            </span>
+          </Button>
+        </motion.div>
+        <motion.div
+          initial={{ y: "10%", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          className="grid grid-cols-1 gap-8 lg:grid-cols-3 xl:px-36">
+          <Card
+            item={{
+              emoji: "🏄🏼‍♂️",
+              title: "Es facil!",
+              description: "Ya que todo es personalizado puedes aprender los temas que quieras cuando quieras",
+            }}
+          />
+          <Card
+            item={{
+              emoji: "🚀",
+              title: "Aprendiendo ingles con ESLIA",
+              description: "Aca podras generar hojas de vocabulario y ejercicios que que te resultara increible para aprender ingles",
+            }}
+          />
+          <Card
+            item={{
+              emoji: "💸",
+              title: "Gratis! ",
+              description: "Puedes generar tantas hojas de ejercicios como quieras",
+            }}></Card>
+        </motion.div>
+      </div>
+    </AnimatePresence>
+  );
 }
 
 //ESLIA
