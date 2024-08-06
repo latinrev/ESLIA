1319a20c76e8f0a802c042d796e51aceb8a7f333
latinrev <latinreve@gmail.com>
2024-08-06 12:59:03 -0500

    feat(analytics): add vercel analytics components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { cn } from "@/utils";
-import { createClient } from "@/utils/supabase/server";
 import Header from "@/components/Header";
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,6 +23,8 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={cn(outfit.className, "bg-bg relative")}>
+        <Analytics />
+        <SpeedInsights />
         <Header />
         <main className="flex h-full min-h-screen flex-col items-center justify-center overflow-hidden bg-bg px-2 py-12 pb-24 sm:px-8 lg:px-20 lg:py-24">
           <Providers>{children}</Providers>
